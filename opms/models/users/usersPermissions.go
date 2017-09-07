119cb21ce1513cb93c93f0509d1afc02c65212be
unknown <zhuhongcheng@360buyAD.local>
2017-09-07 16:14:08 +0800

    first commit

--- opms/models/users/usersPermissions.go
+++ opms/models/users/usersPermissions.go
@@ -4,74 +4,78 @@ import (
 	"fmt"
 	"opms/models"
 	"opms/utils"
+	//"sort"
 	"strings"
 
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 )
 
-type UserPermissions struct {
-	Id          int64 `orm:"pk;column(userid);"`
-	Permissions string
-	Model       string
-	Modelc      string
+type UsersPermissions struct {
+	Id         int64 `orm:"pk;column(userid);"`
+	Permission string
+	Model      string
+	Modelc     string
 }
 
-func (this *UserPermissions) TableName() string {
-	return models.RegisterModel(new(UserPermissions))
+func (this *UsersPermissions) TableName() string {
+	return models.TableName("users_permissions")
+}
+func init() {
+	orm.RegisterModel(new(UsersPermissions))
 }
 
-func GetPermission(id int64) string {
+func GetPermissions(id int64) string {
 	var err error
 	var name string
 	err = utils.GetCache("GetPermissionsName.id."+fmt.Sprintf("%d", id), &name)
 	if err != nil {
 		cache_expire, _ := beego.AppConfig.Int("cache_expire")
-		var permission UserPermissions
+		var permission UsersPermissions
 		o := orm.NewOrm()
-		o.QueryTable(models.TableName("users_permissions")).Filter("userid, id").One(&permission, &permission)
-		name = permission.Permissions
-		utils.SetCache("GetPermission.id."+fmt.Sprintf("%d", id), name, cache_expire)
+		o.QueryTable(models.TableName("users_permissions")).Filter("userid", id).One(&permission, "permission")
+		name = permission.Permission
+		utils.SetCache("GetPermissions.id."+fmt.Sprintf("%d", id), name, cache_expire)
 	}
 	return name
 }
 
-type UserPermissionsAll struct {
+type UsersPermissionsAll struct {
 	Permission string
 	Groupid    string
 }
 
-func GetPermission(id int64) (UserPermissionsAll, error) {
-	var pers []UserPermissions
+func GetPermissionsAll(id int64) (UsersPermissionsAll, error) {
+	var pers []UsersPermissionsAll
 	var err error
 
 	qb, _ := orm.NewQueryBuilder("mysql")
-	qb.Select("p.ename As permission", "gu.groupid").From("pms_groups_user As gu")
-	LeftJoin("pms_groups_permission AS gp").On("gp.groupid = gu.groupid")
-	LeftJoin("pms_permissions AS p").On("p.permissionid = gp.permissionid")
-	Where("gu.userid=?")
+
+	qb.Select("p.ename AS permission", "gu.groupid").From("pms_groups_user AS gu").
+		LeftJoin("pms_groups_permission AS gp").On("gp.groupid = gu.groupid").
+		LeftJoin("pms_permissions AS p").On("p.permissionid = gp.permissionid").
+		Where("gu.userid=?")
 	sql := qb.String()
 	o := orm.NewOrm()
 	_, err = o.Raw(sql, id).QueryRows(&pers)
 
-	var perimissionstring = ""
+	var permissionstring = ""
 	var groupidstring = ""
 	for _, v := range pers {
-		perimissionstring += v.Permissions + ","
+		permissionstring += v.Permission + ","
 		groupidstring += v.Groupid + ","
 	}
-	groupidstring := strings.Split(strings.Trim(groupidstring, ","), ",")
-	groupMap := utils.RemoveDuplicatesAndEmpty(groupidstring)
+	groupidstrings := strings.Split(strings.Trim(groupidstring, ","), ",")
+	groupMap := utils.RemoveDuplicatesAndEmpty(groupidstrings)
 	groupidstring = ""
-
+	//sort.Strings(groupMap)
 	for _, v := range groupMap {
 		groupidstring += v + ","
 	}
-	var per UserPermissionsAll
-	per.Permission = strings.Trim(perimissionstring, ",")
+	var per UsersPermissionsAll
+	per.Permission = strings.Trim(permissionstring, ",")
 	per.Groupid = strings.Trim(groupidstring, ",")
 	return per, err
-
 }
 
 func GetPermissionsAllOld(id int64) (UsersPermissions, error) {
